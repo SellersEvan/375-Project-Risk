3d8bbfa92b9de9a8823ef93e140ad783b6647d25
rhit-mazanyam <mazanyam@rose-hulman.edu>
2023-03-29 17:06:26 -0400

    Made getTerritories() in MapManager return a read-only List

--- src/main/java/model/MapManager.java
+++ src/main/java/model/MapManager.java
@@ -2,12 +2,13 @@ package model;
 
 import controller.GameSetup;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MapManager {
     private static final List<Territory> TERRITORIES = GameSetup.initTerritories();
 
     public static List<Territory> getTerritories() {
-        return TERRITORIES;
+        return Collections.unmodifiableList(TERRITORIES);
     }
 }
