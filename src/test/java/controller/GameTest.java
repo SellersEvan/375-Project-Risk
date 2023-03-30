5d39502a2f15416b72a3696c3c84d1e8bae4e8c0
masurhj <masurhj@rose-hulman.edu>
2023-03-30 12:11:24 -0400

    added back changes

--- src/test/java/controller/GameTest.java
+++ src/test/java/controller/GameTest.java
@@ -21,16 +21,16 @@ public class GameTest {
     void testRemoveDefeatedPlayerFromGameLoop0() {
         Game game = new Game(6);
         game.removeDefeatedPlayer(0);
-        assertEquals(game.playerArray.size(), 5);
-        assertEquals(game.numberOfPlayers, 5);
+        assertEquals(game.playerController.playerArray.size(), 5);
+        assertEquals(game.playerController.getNumberOfPlayers(), 5);
     }
 
     @Test
     void testRemoveDefeatedPlayerFromGameLoop5() {
         Game game = new Game(6);
         game.removeDefeatedPlayer(5);
-        assertEquals(game.playerArray.size(), 5);
-        assertEquals(game.numberOfPlayers, 5);
+        assertEquals(game.playerController.playerArray.size(), 5);
+        assertEquals(game.playerController.getNumberOfPlayers(), 5);
     }
 
     @Test
@@ -46,38 +46,38 @@ public class GameTest {
         EasyMock.expect(r.nextInt(6)).andReturn(3);
         EasyMock.replay(r);
         game.setFirstPlayer(r);
-        assertEquals(game.current, 3);
+        assertEquals(game.playerController.currentPlayer, 3);
         EasyMock.verify(r);
     }
 
     @Test
     void testNextTurn() {
         Game game = new Game(6);
-        game.current = 0;
+        game.playerController.currentPlayer = 0;
         game.nextPlayer();
-        assertEquals(game.current, 1);
+        assertEquals(game.playerController.currentPlayer, 1);
     }
 
     @Test
     void testNextTurn1() {
         Game game = new Game(6);
-        game.current = 1;
+        game.playerController.currentPlayer = 1;
         game.nextPlayer();
-        assertEquals(game.current, 2);
+        assertEquals(game.playerController.currentPlayer, 2);
     }
 
     @Test
     void testNextTurn5() {
         Game game = new Game(6);
-        game.current = 5;
+        game.playerController.currentPlayer = 5;
         game.nextPlayer();
-        assertEquals(game.current, 0);
+        assertEquals(game.playerController.currentPlayer, 0);
     }
 
     @Test
     void testInitGamePlayerArray() {
         Game g = new Game(6);
-        assertEquals(g.gameSetup.getArmiesPerPlayer(), g.playerArray.get(0).getArmiesAvailable());
+        assertEquals(g.gameSetup.getArmiesPerPlayer(), g.playerController.playerArray.get(0).getArmiesAvailable());
     }
 
 }
