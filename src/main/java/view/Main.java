75926b89edbf4e4ceeccdbe2b0e89569106fec78
Evan Sellers <github@sellersew.com>
2023-04-19 10:59:30 -0400

    Added Development Startup

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,20 +1,51 @@
 package view;
 
 import controller.Game;
+import model.CardTrader;
 import model.Player;
 import java.util.*;
 import model.Map.World;
+import model.PlayerColor;
 
 
 public class Main {
 
 
+    private static boolean SETUP_MODE_PRODUCTION = false;
+
+
     public static void main(String[] args) {
+        if (SETUP_MODE_PRODUCTION) {
+            setupProduction();
+        } else {
+            setDevelopment();
+        }
+    }
+
+
+    private static void setupProduction() {
         Setup.setupProperties();
         ResourceBundle bundle = Setup.selectLanguage();
         World world           = Setup.selectWorld(bundle);
         int numberOfPlayers   = Setup.selectNumberOfPlayers(bundle);
         List<Player> players  = Setup.setupPlayers(bundle, numberOfPlayers);
+
+        Game game = new Game(world, players);
+        game.setupUI(bundle, GameView.class);
+        game.begin();
+    }
+
+
+    private static void setDevelopment() {
+        Setup.setupProperties();
+        ResourceBundle bundle = ResourceBundle.getBundle("messages_en");
+        World world           = new World(World.getMapFiles().get("Earth"));
+
+        Random random         = new Random();
+        CardTrader cardTrader = new CardTrader();
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(PlayerColor.GREEN, "Mr. Green", random, cardTrader));
+        players.add(new Player(PlayerColor.BLUE, "Mrs. Peacock", random, cardTrader));
 
         Game game = new Game(world, players);
         game.setupUI(bundle, GameView.class);
